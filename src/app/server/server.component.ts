abd9dfbf5b8c497138a03527ef363a77ee63c881
vrivas <vrivas@teravisiontech.com>
2017-06-09 13:16:50 -0400

    Creating nested components

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by victor on 07/06/17.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent{
+
+}
